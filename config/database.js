544ef408d58ed5046377a8f317c562cd12afa30d
ANCELIN DOUGLAS <ancelindouglas@gmail.com>
2022-11-28 17:04:56 +0100

    Save

--- config/database.js
+++ config/database.js
@@ -5,8 +5,6 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'doug'),
-      // user: env('DATABASE_USERNAME', 'Doug'),
-      // password: env('DATABASE_PASSWORD', '56260169'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
