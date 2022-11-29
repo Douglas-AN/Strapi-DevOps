5367e663ec388e7a6fe94fe637f189caab4eaa1a
ANCELIN DOUGLAS <ancelindouglas@gmail.com>
2022-11-29 11:41:31 +0100

    ADD Environnement variable

--- config/database.js
+++ config/database.js
@@ -5,6 +5,8 @@ module.exports = ({ env }) => ({
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'doug'),
+      username: env('DATABASE_USER', 'doug'),
+      password: env('DATABASE_PASSWORD', '123456'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
